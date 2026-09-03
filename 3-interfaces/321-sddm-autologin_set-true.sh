0860f9a495835e2d609ce2f2baa973e7c628be29
Raymond Risko <rmd.risko@outlook.com>
2026-09-03 13:44:41 -0300

    Add 321-sddm-autologin_set-true.sh to enable SDDM autologin by setting Autologin User with crudini

---
+++ 3-interfaces/321-sddm-autologin_set-true.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+  echo 'Error, this script need be run as root.'
+  exit 1
+fi
+
+if [ ! -e /usr/bin/crudini ]; then
+  echo 'Error, CrudINI is not installed.'
+  exit 1
+fi
+
+# Set autologin user for SDDM
+# SDDM enables autologin when the [Autologin] User is set
+crudini --set /etc/sddm.conf Autologin User user
+vCrudINI_return=$?
+
+if [ $vCrudINI_return -eq 0 ]; then
+  echo 'Set autologin config in: /etc/sddm.conf'
+else
+  echo 'Error, not set config in: /etc/sddm.conf'
+  exit $vCrudINI_return
+fi
+
+exit 0
